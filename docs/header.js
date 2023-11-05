9c736fe55180a02dbcfffb99cca747c4cb1684ff
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2023-11-05 17:35:54 +0100

    added docs pages integration

---
+++ docs/header.js
@@ -0,0 +1,12 @@
+
+document.write('\
+<header class="sticky" style="width:-webkit-fill-available">\
+    <div class="topnav">\
+        <a href="https://github.com/ChrisMzz/fractal-art"><img style="height:48px" src="images/github-mark.svg"></a>\
+        <a style="padding-top:15px" href="index.html">Home</a>\
+        <a style="padding-top:15px" href="manual.html">Manual</a>\
+        <a style="float:right;padding-top:15px" href="download.html">Download</a>\
+        <span>Fractal Art</span>\
+    </div>\
+</header>'
+)
