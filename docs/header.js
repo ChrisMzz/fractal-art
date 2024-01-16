c4863baaf0808ac9f4b8cee8e72b22a20610f92c
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2024-01-16 19:08:22 +0100

    fixed absolute position header ruining page structure

--- docs/header.js
+++ docs/header.js
@@ -1,6 +1,6 @@
 
 document.write('\
-<header class="sticky" style="width:-webkit-fill-available;position:absolute;top:0px">\
+<header class="sticky" style="width:-webkit-fill-available;">\
     <div class="topnav">\
         <a href="https://github.com/ChrisMzz/fractal-art"><img style="height:48px" src="images/github-mark.svg"></a>\
         <a style="padding-top:15px" href="index.html">Home</a>\
